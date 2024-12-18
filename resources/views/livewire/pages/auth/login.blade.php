725d6dc7d6bd56f3e01a9a6fd1255389c17ccdee
ViniciusCSantos <viniciuscsantosoficial@gmail.com>
2024-12-18 13:19:39 -0300

    Adiciona botão para criar conta

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -67,5 +67,12 @@ new #[Layout('layouts.guest')] class extends Component
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
+
+        
+        <div class="flex items-center justify-end mt-4">
+            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}" wire:navigate>
+                {{ __('Ainda não possui uma conta?') }}
+            </a>
+        </div>
     </form>
 </div>
