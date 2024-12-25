814ac598f78ed2e252edab16cf5472a677edf155
ViniciusCSantos <viniciuscsantosoficial@gmail.com>
2024-12-25 12:05:37 -0300

    Corrige erro do Flowbite
    Os Componentes do flowbite são atualizados quando há uma atualização atravéz do livewire

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,15 @@
 import './bootstrap';
 import "flowbite";
+import { initFlowbite } from 'flowbite';
+
+Livewire.hook('commit', ({ component, commit, respond, succeed, fail }) => {
+    succeed(({ snapshot, effect }) => {
+        setTimeout(() => {
+            initFlowbite();
+        }, 100); // Adjust the delay as needed
+    })
+})
+
+document.addEventListener('livewire:navigated', () => {
+    initFlowbite();
+});
