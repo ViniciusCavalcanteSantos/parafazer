2e46e50744026ecda1de337165bde032b4924baf
ViniciusCSantos <viniciuscsantosoficial@gmail.com>
2024-12-27 10:30:35 -0300

    Renomeia o id do accordion

--- resources/views/livewire/pages/todo/todo-groups.blade.php
+++ resources/views/livewire/pages/todo/todo-groups.blade.php
@@ -51,9 +51,11 @@ new #[Layout('layouts.app')] class extends Component
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    @forEach($todoGroups as $todoGroup)
-                        <x-todo.todo-group-list :$todoGroup />
-                    @endForEach
+                    <div id="accordion-todo-group" data-accordion="open">
+                        @forEach($todoGroups as $todoGroup)
+                            <x-todo.todo-group-list :$todoGroup />
+                        @endForEach
+                    </div>
                 </div>
             </div>
         </div>
