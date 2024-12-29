feb18d0acd9681fd3463fa5ae7bfb4f150daf73e
ViniciusCSantos <viniciuscsantosoficial@gmail.com>
2024-12-29 19:05:08 -0300

    Adiciona botão de remover tarefa

--- resources/views/components/tasks/tasks-item.blade.php
+++ resources/views/components/tasks/tasks-item.blade.php
@@ -8,24 +8,49 @@ new class extends Component {
 }; ?>
 
 <div wire:key="{{ $task->id }}">
-    <h2 id="accordion-task-item-heading-{{ $task->id }}">
-        <button type="button"
-                class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
-                data-accordion-target="#accordion-task-item-body-{{ $task->id }}" aria-expanded="false"
-                aria-controls="accordion-task-item-body-3">
-            <span>{{ $task->title }}</span>
-            <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
-                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+    <div class="relative">
+        <h2 id="accordion-task-item-heading-{{ $task->id }}">
+            <button type="button"
+                    class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+                    data-accordion-target="#accordion-task-item-body-{{ $task->id }}" aria-expanded="false"
+                    aria-controls="accordion-task-item-body-3">
+                <span>{{ $task->title }}</span>
+            </button>
+        </h2>
+
+        <button id="task-item-dropdown-button-{{ $task->id }}" data-dropdown-toggle="task-item-dropdown-{{ $task->id }}"
+                class="absolute top-1/2 -translate-y-1/2 right-1 text-white font-medium text-sm px-5 py-2.5 text-center"
+                type="button" title="Menu Suspenso">
+            <svg class="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                 viewBox="0 0 10 6">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M9 5 5 1 1 5" />
+                      d="m1 1 4 4 4-4"/>
             </svg>
         </button>
-    </h2>
-    <div id="accordion-task-item-body-{{ $task->id }}" class="hidden" aria-labelledby="accordion-task-item-heading-{{ $task->id }}">
+
+        <!-- Dropdown menu -->
+        <div id="task-item-dropdown-{{ $task->id }}"
+             class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700">
+            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200"
+                aria-labelledby="task-item-dropdown-button-{{ $task->id }}">
+                <li>
+                    <button wire:click="removeTask({{ $task->id }})"
+                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white text-center">
+                        Apagar
+                    </button>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+
+    <div id="accordion-task-item-body-{{ $task->id }}" class="hidden"
+         aria-labelledby="accordion-task-item-heading-{{ $task->id }}">
         <div class="text-center p-5 border border-t-0 border-gray-200 dark:border-gray-700">
             <p class="mb-4 text-gray-500 dark:text-gray-400">Sem Subtarefas 😢</p>
 
-            <button type="button" class="text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 focus:outline-none dark:focus:ring-indigo-800">
+            <button type="button"
+                    class="text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 focus:outline-none dark:focus:ring-indigo-800">
                 Adicionar uma Subtarefa
             </button>
         </div>
--- resources/views/livewire/pages/tasks/tasks.blade.php
+++ resources/views/livewire/pages/tasks/tasks.blade.php
@@ -9,28 +9,46 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Collection;
 
-new #[Layout('layouts.app')] class extends Component
-{
+new #[Layout('layouts.app')] class extends Component {
     public string $title;
     public int $users_id;
     public Collection $tasks;
 
-    public function mount() {
+    public function mount()
+    {
         $users_id = Auth::user()->id;
         $this->tasks = Task::where("users_id", $users_id)->get();
     }
 
-    public function addTask() {
+    public function addTask()
+    {
         $users_id = Auth::user()->id;
         $this->users_id = $users_id;
         $validated = $this->validate([
             'title' => ['required', 'string', 'min:5', 'max:255'],
-            'users_id' => ['required', 'integer', 'numeric', 'max:255', 'exists:'.User::class.',id'],
+            'users_id' => ['required', 'integer', 'numeric', 'max:255', 'exists:' . User::class . ',id'],
         ]);
 
         Task::create($validated);
-        $this->tasks = Task::where("users_id", $users_id)->get();
+        $this->updateUserTasks();
         $this->dispatch('close-modal', 'add-task');
+    }
+
+    public function removeTask($taskId)
+    {
+        $task = Task::where("id", $taskId)->get()->first();
+        $users_id = Auth::user()->id;
+
+        if ($task->users_id === $users_id) {
+            $task->delete();
+            $this->updateUserTasks();
+        }
+    }
+
+    public function updateUserTasks()
+    {
+        $users_id = Auth::user()->id;
+        $this->tasks = Task::where("users_id", $users_id)->get();
     }
 }; ?>
 
@@ -38,9 +56,9 @@ new #[Layout('layouts.app')] class extends Component
     <div class="pt-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <button type="button"
-                x-data=""
-                x-on:click.prevent="$dispatch('open-modal', 'add-task')"
-                class="text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 focus:outline-none dark:focus:ring-indigo-800"
+                    x-data=""
+                    x-on:click.prevent="$dispatch('open-modal', 'add-task')"
+                    class="text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 focus:outline-none dark:focus:ring-indigo-800"
             >
                 Adicionar Tarefa
             </button>
@@ -49,11 +67,11 @@ new #[Layout('layouts.app')] class extends Component
 
     <div class="py-4">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <div id="accordion-tasks" data-accordion="open">
                         @forEach($tasks as $task)
-                            <x-tasks.tasks-item :$task />
+                            <x-tasks.tasks-item :$task/>
                         @endForEach
                     </div>
                 </div>
@@ -64,12 +82,12 @@ new #[Layout('layouts.app')] class extends Component
     <x-modal name="add-task" :show="$errors->isNotEmpty()" focusable>
         <form wire:submit="addTask" class="p-6">
             <div>
-                <x-input-label for="title" :value="__('Titulo')" />
+                <x-input-label for="title" :value="__('Titulo')"/>
                 <x-text-input wire:model="title" id="title" class="block mt-1 w-full"
-                                type="text"
-                                name="title"
-                                required autofocus />
-                <x-input-error :messages="$errors->get('title')" class="mt-2" />
+                              type="text"
+                              name="title"
+                              required autofocus/>
+                <x-input-error :messages="$errors->get('title')" class="mt-2"/>
             </div>
 
             <div class="flex items-center justify-end mt-4">
